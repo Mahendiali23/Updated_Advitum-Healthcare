8c883290f0995a97586c37aee476b210249ea21a
dell <karadiyahaiderali7@gmail.com>
2026-03-12 22:49:51 +0530

    Updating files to friend's repository

--- stores/urls.py
+++ stores/urls.py
@@ -7,13 +7,19 @@ urlpatterns = [
     path('', first, name='first'),
     path('home', home, name='home'),
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 86c38f3 (Updating files to friend's repository)
     path('product', product, name='products'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+<<<<<<< HEAD
 =======
   path('product', product, name='products'),
   path('about', about, name='about'),
   path('contact', contact, name='contact'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 >>>>>>> 0a42598d478252253a86aabd558b58b3ed4bed23
+=======
+>>>>>>> 86c38f3 (Updating files to friend's repository)
